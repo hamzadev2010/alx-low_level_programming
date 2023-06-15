c6666f1bfaba1e7af6eba4afce58c08ed9f7a0f6
hamza <hamzachanik@outlook.com>
2023-06-15 13:41:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,12 @@ printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+#ifdef __x86_64__
+printf("System is 64-bit\n");
+#elif defined(__i386__)
+printf("System is 32-bit\n");
+#else
+printf("System architecture is unknown\n");
+#endif
 return (0);
 }
