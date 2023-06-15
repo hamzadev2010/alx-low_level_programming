6e268346c07f046d991504a852bf9f100fbca75b
hamza <hamzachanik@outlook.com>
2023-06-15 12:29:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <studio.h>
+/**
+ * main - A programme print a line with function
+* Return :0 ( success)
+*/
+int main(void)
+{
+putes ("\"Programming is like building a multilingual puzzle");
+return (0) ;
+}
