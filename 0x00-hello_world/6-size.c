4d0232057d07090b27103a2bcc432e760b9d21af
hamza <hamzachanik@outlook.com>
2023-06-15 16:32:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- *
- * Return: 0 (Success)
- */
+ * main - A programme print  the size of various comp 
+* Return :0 ( success) 
+*/
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("Size of char: %d  byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of int: %d  byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of long int: %d byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of long long int: %d byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of float: %d  byte(s)\n", (unsigned long)sizeof(f));
-#if defined(__LP64__) || defined(_LP64)
-printf("Machine architecture: 64-bit\n");
-#else
-printf("Machine architecture: 32-bit\n");
-#endif
-return (0); }
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int)); 
+printf("Size of a long long int: %lu byte(s)\n",  sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
+}
