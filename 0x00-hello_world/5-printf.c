31a9cca9634caaf484f0016c3897fb4f40ec205d
hamza <hamzachanik@outlook.com>
2023-06-15 13:16:14 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+/**
+ * main - A programme print  the size of various comp 
+* Return :0 ( success) 
+*/
+int main(void) 
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("Size of char: %lu byte(s)\n",(unisgned long)sizeof(a);
+printf ( "Size of int: %lu byte(s)\n",(unisgned long)sizeof(b);
+printf ( "Size of long int: %lu byte(s)\n",(unisgned long)sizeof(c);
+printf ( "Size of long long int: %lu byte(s)\n",(unisgned long)sizeof(d);
+printf ( "Size of float: %lu byte(s)\n",(unisgned long)sizeof(f);
+return (0);
+}
