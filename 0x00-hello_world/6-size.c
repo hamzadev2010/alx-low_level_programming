69134c2e3b83178d6b2f6e847cc1ce3424c4da6c
hamza <hamzachanik@outlook.com>
2023-06-15 14:06:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,4 +16,9 @@ printf("Size of int: %d  byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long int: %d byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %d byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of float: %d  byte(s)\n", (unsigned long)sizeof(f));
+#if defined(__LP64__) || defined(_LP64)
+printf("Machine architecture: 64-bit\n");
+#else
+printf("Machine architecture: 32-bit\n");
+#endif
 return (0); }
