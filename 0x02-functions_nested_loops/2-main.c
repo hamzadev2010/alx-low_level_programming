c2deb639b7d625cf0a55056ca30958d65a56bee8
hamza <hamzachanik@outlook.com>
2023-06-20 09:09:37 -0700

    task 3

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Calls the print_alphabet_x10 function to print the alphabet
+ *              10 times, followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
