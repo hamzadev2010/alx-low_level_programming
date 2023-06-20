9523c3e8d6ec507b826b163fd6a6397aaee73383
hamza <hamzachanik@outlook.com>
2023-06-20 09:04:49 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Calls the print_alphabet function to print all alphabets
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
