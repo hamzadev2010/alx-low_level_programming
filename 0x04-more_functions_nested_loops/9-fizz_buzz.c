7f6116a4d12f6624c1a574fa6fd7c95f6d735e60
hamza <hamzachanik@outlook.com>
2023-06-22 11:21:08 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,42 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Prints "Fizz" for numbers divisible by 3, "Buzz" for numbers divisible by 5,
- *        and "FizzBuzz" for numbers divisible by both 3 and 5.
+ * main - Prints the numbers from 1 to 100, followed by a new line.
+ *        For multiples of three, prints "Fizz" instead of the number.
+ *        For multiples of five, prints "Buzz" instead of the number.
+ *        For numbers that are multiples of both three and five, prints "FizzBuzz".
  *
- * Return: Always 0.
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf(" FizzBuzz");
-		}
-		else if (n % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
 		}
-		else if (n % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
 		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
 		else
 		{
-			printf(" %d", n);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
+
 	return (0);
 }
