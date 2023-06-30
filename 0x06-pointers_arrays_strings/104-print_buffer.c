2ece64b42e798abfd0ce24f3de2596ebc6b76a68
hamza <hamzachanik@outlook.com>
2023-06-30 13:00:24 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,6 +10,7 @@
  */
 void print_buffer(char *b, int size)
 {
+
 	int o, j, i;
 
 	o = 0;
@@ -22,35 +23,23 @@ void print_buffer(char *b, int size)
 
 	while (o < size)
 	{
-		j = size - o < 10 ? size - o : 10;
+		j = (size - o < 10) ? size - o : 10;
 		printf("%08x: ", o);
 
 		for (i = 0; i < 10; i++)
 		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf("  ");
-
-			if (i % 2)
-			{
-				printf(" ");
-			}
+			printf(i < j ? "%02x" : "  ");
+			printf(i % 2 ? " " : "");
 		}
 
 		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
 
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
+			printf((c < 32 || c > 132) ? "." : "%c", c);
 		}
 
 		printf("\n");
 		o += 10;
 	}
 }
-
