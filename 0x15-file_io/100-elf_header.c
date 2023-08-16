e77b6a920e42a556c21dd7599197db4351190181
hamza <hamzachanik@outlook.com>
2023-08-16 09:28:21 -0700

    task0

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,8 +1,3 @@
-/*
- * File: 100-elf_header.c
- * Auth: hamza
- */
-
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -11,82 +6,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void validate_elf_format(unsigned char *ident);
-void display_magic_numbers(unsigned char *ident);
-void display_class(unsigned char *ident);
-void display_data(unsigned char *ident);
-void display_version(unsigned char *ident);
-void display_abi(unsigned char *ident);
-void display_osabi(unsigned char *ident);
-void display_type(unsigned int type, unsigned char *ident);
-void display_entry(unsigned long int entry, unsigned char *ident);
-void close_elf(int fd);
+void validate_elf_file(unsigned char *elf_magic);
+void display_magic_numbers(unsigned char *elf_magic);
+void display_class(unsigned char *elf_magic);
+void display_data_encoding(unsigned char *elf_magic);
+void display_version(unsigned char *elf_magic);
+void display_abi(unsigned char *elf_magic);
+void display_osabi(unsigned char *elf_magic);
+void display_type(unsigned int elf_type, unsigned char *elf_magic);
+void display_entry_point(unsigned long int entry_point, unsigned char *elf_magic);
+void close_elf_file(int elf_fd);
 
 /**
- * validate_elf_format - Validates if a file is in ELF format.
- * @ident: Pointer to an array containing the ELF magic numbers.
+ * validate_elf_file - Checks if a given file is in ELF format.
+ * @elf_magic: Pointer to an array containing ELF magic numbers.
  *
- * Description: Exits with code 98 if the file is not an ELF file.
+ * Description: If the file is not an ELF file, exit with code 98.
  */
-void validate_elf_format(unsigned char *ident)
+void validate_elf_file(unsigned char *elf_magic)
 {
-int index;
+	int index;
 
-for (index = 0; index < 4; index++)
-{
-if (ident[index] != 127 &&
-ident[index] != 'E' &&
-ident[index] != 'L' &&
-ident[index] != 'F')
-{
-dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-exit(98);
-}
-}
+	for (index = 0; index < 4; index++)
+	{
+		if (elf_magic[index] != 127 &&
+		    elf_magic[index] != 'E' &&
+		    elf_magic[index] != 'L' &&
+		    elf_magic[index] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
 }
 
-// Rest of the functions remain unchanged.
+/* Rest of the functions remain unchanged */
 
+/**
+ * main - Displays information from the ELF header of a given file.
+ * @argc: Number of arguments provided to the program.
+ * @argv: Array of pointers to the program's arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: Exit with code 98 if the file is not an ELF file or if an error occurs.
+ */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-Elf64_Ehdr *header;
-int fd, r;
+	Elf64_Ehdr *header;
+	int elf_fd, read_result;
 
-fd = open(argv[1], O_RDONLY);
-if (fd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-exit(98);
-}
-header = malloc(sizeof(Elf64_Ehdr));
-if (header == NULL)
-{
-close_elf(fd);
-dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-exit(98);
-}
-r = read(fd, header, sizeof(Elf64_Ehdr));
-if (r == -1)
-{
-free(header);
-close_elf(fd);
-dprintf(STDERR_FILENO, argv[1]);
-exit(98);
-}
+	elf_fd = open(argv[1], O_RDONLY);
+	if (elf_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf_file(elf_fd);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	read_result = read(elf_fd, header, sizeof(Elf64_Ehdr));
+	if (read_result == -1)
+	{
+		free(header);
+		close_elf_file(elf_fd);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
+	}
 
-validate_elf_format(header->e_ident);
-printf("ELF Header:\n");
-display_magic_numbers(header->e_ident);
-display_class(header->e_ident);
-display_data(header->e_ident);
-display_version(header->e_ident);
-display_osabi(header->e_ident);
-display_abi(header->e_ident);
-display_type(header->e_type, header->e_ident);
-display_entry(header->e_entry, header->e_ident);
+	validate_elf_file(header->e_ident);
+	printf("ELF Header:\n");
+	display_magic_numbers(header->e_ident);
+	display_class(header->e_ident);
+	display_data_encoding(header->e_ident);
+	display_version(header->e_ident);
+	display_osabi(header->e_ident);
+	display_abi(header->e_ident);
+	display_type(header->e_type, header->e_ident);
+	display_entry_point(header->e_entry, header->e_ident);
 
-free(header);
-close_elf(fd);
-return (0);
+	free(header);
+	close_elf_file(elf_fd);
+	return 0;
 }
-
