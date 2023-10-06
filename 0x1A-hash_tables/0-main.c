32e2fa06c56c2bfe65299a4b74552c4499bca075
hamza <hamzachanik@outlook.com>
2023-10-06 11:15:44 -0700

    task

---
+++ 0x1A-hash_tables/0-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+* main - checking the code for
+*
+* Return: Always EXIT_SUCCESS.
+*/
+int main(void)
+{
+    hash_table_t *ht;
+
+    tab = hash_table_create(1024);
+    printf("%p\n", (void *)tab);
+    return (EXIT_SUCCESS);
+}
