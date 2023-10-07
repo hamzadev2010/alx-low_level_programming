2bab9f05916fea84355e092bfaf6cfa34e2f416e
hamza <hamzachanik@outlook.com>
2023-10-07 15:36:11 -0700

    task

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,28 +1,12 @@
 #include "hash_tables.h"
-/**
-  * hash_table_create - creates a new hash table
-  * @size: array size
-  * Return: pointer for created hash table
-  */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	unsigned int i;
-	hash_table_t *hat = malloc(sizeof(hash_table_t));
 
-	if (size == 0)
-		return (NULL);
-	if (!new_ht)
-		return (NULL);
-	hat->array = malloc(sizeof(hash_node_t *) * size);
-	if (!hat->array)
-	{
-		free(new_ht);
-		return (NULL);
-	}
-	hat->size = size;
-	for (i = 0; i < size; i++)
-	{
-		hat->array[i] = NULL;
-	}
-	return (hat);
+/**
+ * hash_table_get - return the value associated with a key
+ * @ht: the hash table
+ * @key: the key 
+ * Return: value related with element or null 
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
 }
