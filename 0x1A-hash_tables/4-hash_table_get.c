85948c74f414f9a0b8f425c5b75a5d7d6d23a943
hamza <hamzachanik@outlook.com>
2023-10-07 15:42:54 -0700

    task

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,12 +1,29 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_get - return the value associated with a key
- * @ht: the hash table
- * @key: the key 
- * Return: value related with element or null 
- */
+ * hash_table_get - Retrieves the element from the hash table
+ * @ht: The hash table
+ * @key: The key
+* Return: The element retrieved from the table
+*/
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index = 0;
+	hash_node_t *temp = NULL;
 
+	if (!key)
+		return (NULL);
+
+	if (ht)
+	{
+		index = key_index((const unsigned char *)key, ht->size);
+		temp = ht->array[index];
+		while (temp)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return (temp->value);
+			temp = temp->next;
+		}
+	}
+	return (NULL);
 }
