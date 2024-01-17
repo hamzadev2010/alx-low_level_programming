1e2e24eecb9f81114bbb3619fe4707a79143a60b
hamza <hamzachanik@outlook.com>
2024-01-17 14:06:38 -0800

    task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,14 +3,14 @@
 /**
  * function that searches for a value in an array of integers using the Linear search algorithm 
  */
-
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
 	if (!array || size == 0)
 		return (-1);
-for (i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
